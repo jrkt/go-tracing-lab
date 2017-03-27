b05cedfd249c5c6907ed51f01db6a31e78f6d945
Jonathan Stevens <jonathanstevens89@gmail.com>
2017-03-27 15:42:38 -0600

    stuff and things

--- client/client.go
+++ client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	pb "grpc_tracer/proto"
 	"log"
 	"os"
@@ -9,12 +10,18 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/api/option"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+var (
+	headerKey = "stackdriver-trace-context"
 )
 
 func main() {
 
 	// Set up a connection to the server.
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure(), grpc.WithUnaryInterceptor(trace.GRPCClientInterceptor()))
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure(), EnableGRPCTracingDialOption)
+	//conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure(), grpc.WithUnaryInterceptor(trace.GRPCClientInterceptor()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
@@ -42,4 +49,30 @@ func main() {
 	}
 
 	println("Response:", r.Message)
+}
+
+// EnableGRPCTracingDialOption enables tracing of requests that are sent over a gRPC connection.
+var EnableGRPCTracingDialOption = grpc.WithUnaryInterceptor(grpc.UnaryClientInterceptor(clientInterceptor))
+
+func clientInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	// trace current request w/ child span
+	span := trace.FromContext(ctx).NewChild(method)
+	defer span.Finish()
+
+	// new metadata, or copy of existing
+	md, ok := metadata.FromContext(ctx)
+	if !ok {
+		md = metadata.New(nil)
+	} else {
+		md = md.Copy()
+	}
+
+	// append trace header to context metadata
+	// header specification: https://cloud.google.com/trace/docs/faq
+	md[headerKey] = append(
+		md[headerKey], fmt.Sprintf("%s/%d;o=1", span.TraceID(), 0),
+	)
+	ctx = metadata.NewContext(ctx, md)
+
+	return invoker(ctx, method, req, reply, cc, opts...)
 }
--- server/server.go
+++ server/server.go
@@ -5,11 +5,17 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 
 	"cloud.google.com/go/trace"
 	"golang.org/x/net/context"
 	"google.golang.org/api/option"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+var (
+	headerKey = "stackdriver-trace-context"
 )
 
 // server is used to implement helloworld.GreeterServer.
@@ -43,11 +49,36 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	s := grpc.NewServer(grpc.UnaryInterceptor(trace.GRPCServerInterceptor(tc)))
+	s := grpc.NewServer(EnableGRPCTracingServerOption(tc))
 	pb.RegisterGreeterServer(s, &server{})
 
 	println("listening on :50051")
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+// EnableGRPCTracingServerOption enables parsing google trace header from metadata
+// and adds a new child span to the incoming request context.
+func EnableGRPCTracingServerOption(traceClient *trace.Client) grpc.ServerOption {
+	return grpc.UnaryInterceptor(serverInterceptor(traceClient))
+}
+
+func serverInterceptor(traceClient *trace.Client) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (resp interface{}, err error) {
+		// fetch metadata from request context
+		md, ok := metadata.FromContext(ctx)
+		if !ok {
+			md = metadata.New(nil)
+		}
+		header := strings.Join(md[headerKey], "")
+
+		// create new child span from google trace header, add to
+		// current request context
+		span := traceClient.SpanFromHeader(info.FullMethod, header)
+		defer span.Finish()
+		ctx = trace.NewContext(ctx, span)
+
+		return handler(ctx, req)
 	}
 }
